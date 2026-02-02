150f9ffc6369f3b7198b34006c6907178a6e15d3
Dev <dks312603@gmail.com>
2026-02-02 23:43:07 +0530

    expense tracker backend

--- expense-tracker/src/main/java/com/expensetracker/ExpenseTrackerApplication.java
+++ expense-tracker/src/main/java/com/expensetracker/ExpenseTrackerApplication.java
@@ -3,6 +3,8 @@ package com.expensetracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Main entry point for the Expense Tracker Spring Boot application.
+
 @SpringBootApplication
 public class ExpenseTrackerApplication {
 
